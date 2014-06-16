11547c06809364fe3e9c50f147b48a58e8bfc381
Vijay Bhaskar <vijaymca@sify.com>
2014-06-16 19:59:07 +0530

    F&HC
    First

---
+++ fhc.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+	alert('welcome to new concept');
+	loadContent();
+});
+
+function loadContent() {
+
+	$().SPServices({
+		operation : "GetListItems", //Method name
+		async : false,
+		//webURL : webUrl,//pass webUrl dynamically
+		listName : "Regions", // List Name
+		//CAMLQueryOptions : "",
+		//CAMLViewFields : "<ViewFields><FieldRef Name='Title' /></ViewFields>",
+		CAMLQuery : "",
+		//CAMLRowLimit : 1,
+		completefunc : function (xData, Status) {
+
+			alert(xData.responseText);
+
+			if (xData.status == 200) {
+				var myJson = $(xData.responseXML).SPFilterNode("z:row").SPXmlToJson({
+						mapping : {
+							ows_ID : {
+								mappedName : "ID",
+								objectType : "Counter"
+							},
+							ows_Title : {
+								mappedName : "Title",
+								objectType : "Text"
+							}
+						}, // name, mappedName, objectType
+						includeAllAttrs : true
+					});
+				alert(myJson);
+			} else {
+				alert(xData.status);
+			}
+		}
+	});
+}
